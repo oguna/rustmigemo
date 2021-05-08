43cf46871650be3c18cd0dc7c017da4b9c153e93
oguna <nao.lk.118@gmail.com>
2021-05-08 12:10:17 +0900

    Resolved a warning.

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn main() {
     opts.optflag("h", "help", "Show this message.");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
+        Err(f) => panic!("{}", f.to_string()),
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
